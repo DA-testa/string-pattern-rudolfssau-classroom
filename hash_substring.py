76c728dbf36a270531bae5ae7cf837e98c81e85c
rudolfssau-classroom <125464967+rudolfssau-classroom@users.noreply.github.com>
2023-04-16 23:17:51 +0300

    Add files via upload
    Final. Rudolfs Saukums 221RDB085

--- hash_substring.py
+++ hash_substring.py
@@ -14,18 +14,33 @@ def read_input():
 def print_occurrences(izvade):
     print(" ".join(map(str, izvade)))
 
-def get_occurrences(paterna, teksts):
+
+def get_occurrences(paterna, teksts, q, w):
     paradibas = []
-    if len(paterna) > len(teksts):
-        return paradibas
-    pat_hash = hash(paterna)
-    teksta_h = [hash(teksts[i:i + len(paterna)]) for i in range(len(teksts) - len(paterna) + 1)]
-    for i in range(len(teksta_h)):
-        if pat_hash == teksta_h[i] and teksts[i:i + len(paterna)] == paterna:
-            paradibas.append(i)
+    lenTeksts = len(teksts)
+    lenPaterna = len(paterna)
+    a = pow(q, lenPaterna - 1) % w
+    b = t = 0
+    for i in range(lenPaterna):
+        b = (q * b + ord(paterna[i])) % w
+        t = (q * t + ord(teksts[i])) % w
+    for s in range(lenTeksts - lenPaterna + 1):
+        if b == t:
+            match = True
+            for i in range(lenPaterna):
+                if paterna[i] != teksts[s + i]:
+                    match = False
+                    break
+            if match:
+                paradibas.append(s)
+        if s < lenTeksts - lenPaterna:
+            t = (t - a * ord(teksts[s])) % w
+            t = (t * q + ord(teksts[s + lenPaterna])) % w
+            t = (t + w) % w
     return paradibas
 
 if __name__ == "__main__":
+    q, w = 256, 13
     paterna, teksts = read_input()
-    paradibas = get_occurrences(paterna, teksts)
+    paradibas = get_occurrences(paterna, teksts, q, w)
     print_occurrences(paradibas)
